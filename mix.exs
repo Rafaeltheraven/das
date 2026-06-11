398df310c0fcf0ae1e7560bb792410bf961c2308
Rafaeltheraven <rafael@raaf.nu>
2026-06-11 20:25:22 +0200

    update all the libraries

--- mix.exs
+++ mix.exs
@@ -31,20 +31,20 @@ defmodule Das.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.8"},
-      {:ecto_sql, "~>3.0"},
-      {:myxql, "~>0.6.3"},
-      {:postgrex, "~>0.16.0"},
-      {:ecto_sqlite3, "~>0.9.0"},
-      {:bcrypt_elixir, "~> 3.0"},
-      {:plug_cowboy, "~> 2.0.0"},
-      {:jason, "~> 1.2"},
+      {:ecto, "~> 3.14.0"},
+      {:ecto_sql, "~>3.14.0"},
+      {:myxql, "~>0.9.0"},
+      {:postgrex, "~>0.22.2"},
+      {:ecto_sqlite3, "~>0.24.0"},
+      {:bcrypt_elixir, "~> 3.3.2"},
+      {:plug_cowboy, "~> 2.8.1"},
+      {:jason, "~> 1.4.5"},
       {:ldap_asn, path: "ldap_asn/", manager: :rebar3},
-      {:joken, "~> 2.6"},
-      {:castore, "~> 1.0"},
-      {:mint, "~> 1.0"},
+      {:joken, "~> 2.6.2"},
+      {:castore, "~> 1.0.19"},
+      {:mint, "~> 1.8.0"},
       {:nimble_totp, "~> 1.0"},
-      {:eqrcode, "~> 0.1.10"},
+      {:eqrcode, "~> 0.2.1"},
     ]
   end
 end
